6b294be770272ebf42ced8d6de47d2206cadf5b8
alanghiese <alan.hiese@gmail.com>
2018-08-30 11:12:07 -0300

    constants class added,File manager ready

---
+++ java_compiler/src/java_compiler/Constants.java
@@ -0,0 +1,5 @@
+package java_compiler;
+
+public class Constants {
+	public String ERR_UPS = "Ups! Algo salio mal";
+}
--- java_compiler/src/java_compiler/FileManager.java
+++ java_compiler/src/java_compiler/FileManager.java
@@ -1,5 +1,67 @@
 package java_compiler;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class FileManager {
+	
+	private Constants CONST = new Constants();
+	
+	
+	public ArrayList<String> loadAndGetSourceCode(String path) {
+		ArrayList<String> code = new ArrayList<>();
+		File sourceCode = new File(path);
+		BufferedReader rd = null;
+		try {
+			// Open the file for reading.
+			rd = new BufferedReader(new FileReader(sourceCode));		             	
+			// Read all contents of the file.
+			String inputLine = null;
+		
+			while((inputLine = rd.readLine()) != null) {
+				code.add(inputLine);
+			}
+		}
+		catch(IOException ex) {
+			System.err.println(this.CONST.ERR_UPS);
+			ex.printStackTrace();
+		}
+		finally {
+			// Close the file.
+			try {
+				rd.close();
+			}
+			catch (IOException ex) {
+				System.err.println(this.CONST.ERR_UPS);
+				ex.printStackTrace();
+			}
+		}			
+		return code;
+	}
+		
+	public void writeFile(String path, ArrayList<String> code) {
+		try {
+			File file = new File(path);
+			// if the file not exists
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			FileWriter fw = new FileWriter(file);
+			for(String line: code) {
+				fw.write(line);
+			}
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(file.getAbsolutePath());
+			bw.close();
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
