009282e01bcca6b9ecc99a4fee02f5d75f0891ff
alanghiese <alan.hiese@gmail.com>
2018-08-30 10:53:58 -0300

    symbolTable ready

--- java_compiler/src/java_compiler/SymbolTable.java
+++ java_compiler/src/java_compiler/SymbolTable.java
@@ -6,4 +6,12 @@ public class SymbolTable {
 
 	HashMap<String,Integer> symbolTable = new HashMap<>();
 	
+	public void addSymbol(String symbol, Integer code){
+		this.symbolTable.put(symbol, code);
+	}
+	
+	public Integer getCode(String symbol) {
+		return this.symbolTable.get(symbol);
+	}
+	
 }
