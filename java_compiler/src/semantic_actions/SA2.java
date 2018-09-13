29cd51ac94952634433d7aae703d6d59a143bb92
alanghiese <alan.hiese@gmail.com>
2018-09-13 12:00:06 -0300

    -

--- java_compiler/src/semantic_actions/SA2.java
+++ java_compiler/src/semantic_actions/SA2.java
@@ -1,6 +1,7 @@
 package semantic_actions;
 
 import java.io.BufferedReader;
+import utilities.Constants;
 import java.io.IOException;
 
 public class SA2 extends SemanticAction{
@@ -13,12 +14,23 @@ public class SA2 extends SemanticAction{
 			StringBuilder sAux = new StringBuilder();
 			for (int i = 0; i < buffer.length()-3; i++)
 				sAux.append(buffer.charAt(i));
-			Integer integer = Integer.parseInt(sAux.toString());
+			Integer number = Integer.parseInt(sAux.toString());
 			double MAX_U = Math.pow(2, 16); //max unsigned integer
-			if (integer >= 0 && integer <= MAX_U)
+			if (number >= 0 && number <= MAX_U)
 				return;
 			else
-				token = new Integer(-1);
+				token = new Integer(Constants.ERR_TOKEN);
+		}
+		else if (buffer.charAt(buffer.length()-1) == 'l') {
+			StringBuilder sAux = new StringBuilder();
+			for (int i = 0; i < buffer.length()-2; i++)
+				sAux.append(buffer.charAt(i));
+			Double number = (double) Integer.parseInt(sAux.toString());
+			
+			if (number >= 0 && number <= Integer.MAX_VALUE+1)
+				return;
+			else
+				token = new Integer(Constants.ERR_TOKEN);
 		}
 				
 	}
--- java_compiler/src/utilities/Constants.java
+++ java_compiler/src/utilities/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
 	public static String PATH = "/home/alan/Desktop/FACULTAD/tp_scripting/para hacer pruebas/";
 	public static String INPUT = "code.txt";
 	public static String OUTPUT = "finalCode.txt";
+	public static Integer ERR_TOKEN = -1;
 }
