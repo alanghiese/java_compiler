8117484b6a0412be50ef3d2ea320b70e375704c0
luezgj <38480315+luezgj@users.noreply.github.com>
2018-09-13 12:09:09 -0300

    Update LexicalAnalizer.java
    Clase implementada

--- java_compiler/src/java_compiler/LexicalAnalizer.java
+++ java_compiler/src/java_compiler/LexicalAnalizer.java
@@ -1,5 +1,49 @@
 package java_compiler;
 
-public class LexicalAnalizer {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+
+public class LexicalAnalizer {
+    BufferedReader fileBuffer;
+    TransitionTable transitions= new TransitionTable();
+
+    public LexicalAnalizer(String path) {
+        try {
+            fileBuffer= new BufferedReader(new FileReader(new File(path)));
+        } catch (FileNotFoundException ex) {
+            System.err.println("No se pudo leer el archivo");
+        }
+    }
+    
+    
+    public int yylex(){
+        int status=0;
+        char nextChar;
+        Integer token=null;
+        
+        StringBuilder readed= new StringBuilder();
+        
+        try {
+            nextChar=(char) fileBuffer.read();
+            readed.append(nextChar);
+        } catch (IOException ex) {
+            System.err.println("No se pudo leer caracter");
+        }
+        
+        
+        
+        while (status != TransitionTable.FINAL_ST){
+            transitions.getAction(status, nextChar).excecute(readed,fileBuffer,token);
+            status= transitions.getNextState(status, nextChar);
+            nextChar=readed.charAt(readed.length()-1);
+        }
+        
+        return token;
+    }
+    
 }
+
